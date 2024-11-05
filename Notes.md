4899034b88742093bab5328ea161509443527bcc
stu221 <stu221@pilogin2.pi.sjtu.edu.cn>
2024-11-05 10:57:07 +0800

    notebook

---
+++ Notes.md
@@ -0,0 +1,1 @@
+gpu: `srun --partition=dgx2 --time=12:00:00 --nodes=1 --ntasks=1 --cpus-per-task=4 --mem=16G --gres=gpu:1 --pty /bin/bash`
---
+++ run.sh
@@ -0,0 +1,5 @@
+module load miniconda3/4.8.2
+conda create -n UI
+conda activate UI
+pip install -r PC-Agent/requirements.txt
+sleep 500000
